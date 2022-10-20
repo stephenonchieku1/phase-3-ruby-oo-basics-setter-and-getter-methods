0e4be48828fde9a215eccb70da5b6a08bf841866
stephenonchieku1 <onchiekustephen901@gmail.com>
2022-10-20 18:04:40 +0300

    done

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,16 @@
 class Person
-
-end
+    def name=(name)
+        @name=name
+    
+    end
+    def name
+        @name
+    end
+    
+    def job=(job)
+        @job=job
+    end
+    def job
+        @job
+    end
+ end
